70ff5e6ba4ce3a79c274b60f19608be88d388d9a
Kwang Un Jin <operator1732@gmail.com>
2013-01-09 23:11:45 +0900

    Initial Commit

---
+++ Designated/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Designated
+//
+//  Created by Kwang Un Jin on 13. 1. 9..
+//  Copyright (c) 2013년 Kwang Un Jin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
